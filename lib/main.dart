349bd4c68679c65a6e0664cb167a4e164ccb8501
Maxim <rfmakar@gmail.com>
2025-08-17 10:03:13 +0300

    ver. 1.0.1(4)

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:stories_client/core/functions/di_stories_admin.dart';
 import 'package:stories_data/index.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   final apiKey = dotenv.env['API_KEY'] ?? '';
   await StoriesData.init(apiKey: apiKey);
